1060126576e342aff0003b389b54665608fd2c7b
Rozi Wrya <154524883+RoziW@users.noreply.github.com>
2024-10-31 00:37:54 +0300

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tt;
+
+/**
+ *
+ * @author HB LAPTOP STORE
+ */
+public class NewClass {
+    
+    public void hello(){
+        System.out.println("Hello all");
+    }
+    
+}
