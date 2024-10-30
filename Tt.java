cba919fda7387f3faade53e946122589216308b1
Rozi Wrya <154524883+RoziW@users.noreply.github.com>
2024-10-31 00:37:15 +0300

    Add files via upload

---
+++ Tt.java
@@ -0,0 +1,13 @@
+
+package tt;
+
+public class Tt {
+
+    public static void main(String[] args) {
+       NewClass obj =new NewClass();
+       
+       System.out.println("This is my first code.");
+       obj.hello();
+    }
+    
+}
